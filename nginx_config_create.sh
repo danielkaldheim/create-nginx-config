b9753554e94a9fbb13818264c6a01ddedbbf6ed0
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2014-03-24 09:47:51 +0100

    Added shell script

---
+++ nginx_config_create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Save script folder
+SCRIPT_DIR=$(cd "$(dirname ${BASH_SOURCE[0]})"; pwd)
+
+SITES_AVAILABLE=/usr/local/etc/nginx/sites-available
+SITES_ENABLED=/usr/local/etc/nginx/sites-enabled
+
+cd $SITES_AVAILABLE
+
+CONF_NAME="${2}.conf"
+
+# Add composer.json
+cp "$SCRIPT_DIR/assets/nginx_vhost_wp.conf" $CONF_NAME
+
+# Fix root
+# sed -i '' "s/root \/var\/www/root ${1}/" $CONF_NAME;
+
+# Fix domain
+sed -i '' "s/localhost/${2}/" $CONF_NAME;
+
+# Enable site
+ln -s $SITES_AVAILABLE/$CONF_NAME $SITES_ENABLED/$CONF_NAME
+
+# Stop nginx
+sudo launchctl unload /Library/LaunchDaemons/homebrew.mxcl.nginx.plist
+
+# Start nginx
+sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.nginx.plist
