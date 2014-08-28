cc22b91eb6d98b198100b8d8dc9531b5fab6e364
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2014-08-28 11:19:22 +0200

    Starting on a backup script

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/.bash_ncreate_config
+
+if [ ! -d "${BACKUPPATH}" ]; then
+	mkdir -v -m 755 "${BACKUPPATH}"
+fi
+
+# backup mysql
+
+#TARFILE="mysql_"$(date +'%m-%d-%Y')".tar.bz2"
+TARFILE="mysql.tar.bz2"
+
+mysql --user=$NCREATE_MYSQL_USER --password=$NCREATE_MYSQL_PASSWORD -e 'show databases' | while read dbname; do mysqldump -uroot --password=$NCREATE_MYSQL_PASSWORD --complete-insert $dbname > "${BACKUPPATH}/mysql/${dbname}.sql"; done
+
+find "${BACKUPPATH}/mysql" -name "*.sql" | tar -cjf "/tmp/${TARFILE}" -T -
+cd "${BACKUPPATH}/mysql" && rm -f *.sql
+mv "/tmp/${TARFILE}" "${BACKUPPATH}/"
